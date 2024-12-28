63014c361513272d9e94eca724e515c0ba20cb3e
Anvesh <anveshrahulak105@gmail.com>
2024-12-28 11:10:21 +0530

    initial commit

---
+++ number_guessing_game/number.py
@@ -0,0 +1,15 @@
+import random
+number=random.randint(1,100)
+guess=0
+print("Let's begin the game!!")
+while(guess!=number):
+    guess=int(input("Enter a number:"))
+    if(guess>number):
+        print("Enter a lower value")
+        # guess=guess+1
+    elif(guess<number):
+        print("Enter a higher value")
+        # guess=guess+1
+    else:
+        print('''!!You guessed  it right!!
+  Congradulations''')
