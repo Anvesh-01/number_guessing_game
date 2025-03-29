5f48b1888e66082fad4d1a4505cd4241b9eec90c
Anvesh-01 <146526766+Anvesh-01@users.noreply.github.com>
2025-03-29 19:42:16 +0530

    Update number.py

--- number_guessing_game/number.py
+++ number_guessing_game/number.py
@@ -3,7 +3,7 @@ number=random.randint(1,100)
 guess=0
 print("Let's begin the game!!")
 while(guess!=number):
-    guess=int(input("Enter a number:"))
+    guess=int(input("Enter a number between 1 to 100:"))
     if(guess>number):
         print("Enter a lower value")
         # guess=guess+1
